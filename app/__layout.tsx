a4e81f2111e39e248ab0fcc04bf03aea480ed0a4
Guilherme Vieira <vieirasilvaguilherme70@gmail.com>
2025-01-17 00:43:13 -0300

    first commit

---
+++ app/__layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router'
+import { StyleSheet } from 'react-native'
+
+const __layout = () => {
+  return <Stack screenOptions={{ headerShown: false }}></Stack>
+}
+
+export default __layout
+
+const styles = StyleSheet.create({})
